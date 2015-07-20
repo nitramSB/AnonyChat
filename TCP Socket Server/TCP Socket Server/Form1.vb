5a38c0f1993ee322b367b9789bd9710dd1aea6ef
nitramSB <martin.brunaes@gmail.com>
2015-07-20 23:53:22 +0200

    Revert "Test"
    This reverts commit 7b77cdf15954fe5189c638447c6cc4e733cc96a0.

--- TCP Socket Server/TCP Socket Server/Form1.vb
+++ TCP Socket Server/TCP Socket Server/Form1.vb
@@ -400,8 +400,15 @@ Public Class Form1
         End Try
     End Sub
 
-   
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'test
+        Try
+
+
+           
+
+        Catch ex As Exception
+
+        End Try
+        
     End Sub
 End Class
